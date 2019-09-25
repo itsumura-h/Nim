673a9c1980b2ed8a7bda30636a13e93bc84a783e
itsumura-h <dumblepy@gmail.com>
2019-09-25 15:45:18 +0900

    insert3つ実装

--- src/allographer/src/allographer/update.nim
+++
@@ -1,4 +0,0 @@
-# This is just an example to get you started. Users of your library will
-# import this file by writing ``import allographer/submodule``. Feel free to rename or
-# remove this file altogether. You may create additional modules alongside
-# this file as required.
